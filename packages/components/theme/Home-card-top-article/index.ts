285250d42dfc14eb1164d756ab23a83268f9d5a0
kele_Bingtang <2456019588@qq.com>
2026-05-16 20:41:01 +0800

    fix(theme): 🐞 修复文件丢失问题

--- packages/components/theme/Home-card-top-article/index.ts
+++
@@ -1,6 +0,0 @@
-import HomeCardTopArticle from "./src/index.vue";
-
-export { HomeCardTopArticle as TkHomeCardTopArticle };
-export default HomeCardTopArticle;
-
-export * from "./src/instance";
--- packages/components/theme/Home-card-top-article/src/instance.ts
+++
@@ -1,3 +0,0 @@
-import type HomeCardTopArticle from "./index.vue";
-
-export type TkHomeCardTopArticleInstance = InstanceType<typeof HomeCardTopArticle>;
